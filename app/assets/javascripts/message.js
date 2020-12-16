fbc2cbd7bd95c03f92db7330d16cde519ebbfd5c
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2020-12-16 13:20:47 +0900

    append

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,61 @@
 $(function(){
-  $('.Form').on('submit', function(e){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="message__box">
+          <div class="user">
+            <div class="user__name">
+              ${message.user_name}
+            </div>
+            <div class="user__time">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="message">
+            <p class="message__content">
+              ${message.content}
+            </p>
+            <img class="message__image" src="${message.image}">
+          </div>
+        </div>`
+      return html;
+    } else {
+      let html =
+      `<div class="message__box">
+        <div class="user">
+          <div class="user__name">
+            ${message.user_name}
+          </div>
+          <div class="user__time">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="message">
+          <p class="message__content">
+            ${message.content}
+          </p>
+        </div>
+      </div>`
+      return html;
+    };
+  }
+  $('.form').on('submit', function(e){
     e.preventDefault()
-      console.log("kamakura")
-  })
-})
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      console.log(data)
+      let html = buildHTML(data);
+      $('.main-chat__message-list').append(html);      
+      $('form')[0].reset();
+    })
+  });
+}); 
