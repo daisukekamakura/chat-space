7107d20b51fd0fd6a02a3c00feab6dd0fba8a588
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2020-12-22 14:09:23 +0900

    syuusei

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -65,27 +65,29 @@ $(function(){
   });
 
   let reloadMessages = function(){
-    let last_message_id = $('.message__box:last').data("message-id") || 0;
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-    
-      if (messages.length !== 0) {
-        let insertHTML = '';
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        $('.main-chat__message-list').append(insertHTML);
-        $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      let last_message_id = $('.message__box:last').data("message-id") || 0;
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+      
+        if (messages.length !== 0) {
+          let insertHTML = '';
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message)
+          });
+          $('.main-chat__message-list').append(insertHTML);
+          $('.main-chat__message-list').animate({ scrollTop: $('.main-chat__message-list')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      });
+    }
   };
   setInterval(reloadMessages, 7000);
 }); 
